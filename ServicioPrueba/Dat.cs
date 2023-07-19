650166e203c531cb9188a3628e6a6200b54e3b6a
megalabsdev <135894273+megalabsdev@users.noreply.github.com>
2023-07-19 09:17:44 -0300

    9:17

--- ServicioPrueba/Dat.cs
+++ ServicioPrueba/Dat.cs
@@ -5,12 +5,12 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-
+using System.Security.Principal;
+using System.Windows.Forms;
 
 public class Dat
 {
-    string connectionString = "Data Source=RYAP021BUE;Initial Catalog=Ax2k9ArgRayPro;Integrated Security=True";
-
+  
 
     public Dat()
 	{
@@ -18,62 +18,75 @@ public class Dat
     }
   
 
-    public static DataTable GetDataTableFromMSSQL(string connectionString, string query)
-    {
-        using (SqlConnection connection = new SqlConnection(connectionString))
-        {
-            connection.Open();
+    public string getConn()         
 
-            using (SqlCommand command = new SqlCommand(query, connection))
-            {
-                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
-                {
-                    DataTable dataTable = new DataTable();
-                    adapter.Fill(dataTable);
-                    connection.Close();
-                    return dataTable;
-                }
-            }
-        }
+    {
+        string connectionString = "Data Source=RYAP021BUE;Initial Catalog=Ax2k9ArgRayPro;Integrated Security=True";
+        SqlConnection connection = new SqlConnection(connectionString);
+        connection.Open();        
+        return connection.State.ToString();
     }
 
 
+    //public static DataTable GetDataTableFromMSSQL(string connectionString, string query)
+    //{
+            
+         
+    //    using (SqlConnection connection = new SqlConnection(connectionString))
+    //    {
+    //        connection.Open();
 
-    public static string ConvertDataTableToJson(DataTable dataTable)
-    {
-        List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
-        foreach (DataRow dataRow in dataTable.Rows)
-        {
-            Dictionary<string, object> row = new Dictionary<string, object>();
-            foreach (DataColumn column in dataTable.Columns)
-            {
-                row[column.ColumnName] = dataRow[column];
-            }
-            rows.Add(row);
-        }
-        return JsonConvert.SerializeObject(rows);
-    }
+    //        using (SqlCommand command = new SqlCommand(query, connection))
+    //        {
+    //            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+    //            {
+    //                DataTable dataTable = new DataTable();
+    //                adapter.Fill(dataTable);
+    //                connection.Close();
+    //                return dataTable;
+    //            }
+    //        }
+    //    }
+    //}
 
 
-    public Object getJSON()
-    {
 
-      
-        DataTable dataTable = GetDataTableFromMSSQL(connectionString, "select s.INVENTSUBBATCHID," +
-            "s.ITEMID, i.ITEMNAME, s.PDSDISPOSITIONCODE, " +
-            "s.INVENTBATCHID,l.PRODDATE,l.EXPDATE," +
-            "im.unitid" +
-            "from InventSubBatch_MPH s" +
-            "inner join INVENTTABLE i on i.ITEMID=s.ITEMID" +
-            "inner join INVENTBATCH l on s.ITEMID=l.ITEMID " +
-            "and s.INVENTBATCHID=l.INVENTBATCHID" +
-            "inner join INVENTTABLEMODULE im on im.ITEMID=i.ITEMID" +
-            "where s.DATAAREAID='070' and l.DATAAREAID='070' and i.DATAAREAID='070'" +
-            "and im.DATAAREAID='070' and im.MODULETYPE=0" +
-            "order by s.INVENTSUBBATCHID");
-        string json = ConvertDataTableToJson(dataTable);
-        return (json);
+    //public static string ConvertDataTableToJson(DataTable dataTable)
+    //{
+    //    List<Dictionary<string, object>> rows = new List<Dictionary<string, object>>();
+    //    foreach (DataRow dataRow in dataTable.Rows)
+    //    {
+    //        Dictionary<string, object> row = new Dictionary<string, object>();
+    //        foreach (DataColumn column in dataTable.Columns)
+    //        {
+    //            row[column.ColumnName] = dataRow[column];
+    //        }
+    //        rows.Add(row);
+    //    }
+    //    return JsonConvert.SerializeObject(rows);
+    //}
 
-    }
-     
+
+    //public string getJSON()
+    //{
+    //    //   DataTable dataTable = GetDataTableFromMSSQL(connectionString, "select * FROM InventSubBatch_MPH");
+
+    //    //DataTable dataTable = GetDataTableFromMSSQL(connectionString, "select s.INVENTSUBBATCHID," +
+    //    //    "s.ITEMID, i.ITEMNAME, s.PDSDISPOSITIONCODE, " +
+    //    //    "s.INVENTBATCHID,l.PRODDATE,l.EXPDATE," +
+    //    //    "im.unitid" +
+    //    //    "from InventSubBatch_MPH s" +
+    //    //    "inner join INVENTTABLE i on i.ITEMID=s.ITEMID" +
+    //    //    "inner join INVENTBATCH l on s.ITEMID=l.ITEMID " +
+    //    //    "and s.INVENTBATCHID=l.INVENTBATCHID" +
+    //    //    "inner join INVENTTABLEMODULE im on im.ITEMID=i.ITEMID" +
+    //    //    "where s.DATAAREAID='070' and l.DATAAREAID='070' and i.DATAAREAID='070'" +
+    //    //    "and im.DATAAREAID='070' and im.MODULETYPE=0" +
+    //    //    "order by s.INVENTSUBBATCHID");
+    //    //  string json = ConvertDataTableToJson(dataTable);
+    //    //   return (json);
+    //    //   return dataTable.ToString();
+
+    //}
+
 }
--- ServicioPrueba/Service1.cs
+++ ServicioPrueba/Service1.cs
@@ -13,14 +13,15 @@ using System.Text;
 using System.Threading;
 using System.Text;
 using Newtonsoft.Json;
+using System.Windows.Forms;
+using System.Web;
 
 namespace ServicioPrueba
 {
     public partial class Service1 : ServiceBase
     {
 
-        // Establece conexion con bb.dd.    
-        Dat datos = new Dat();
+      
 
         public Service1()
         {
@@ -34,7 +35,7 @@ namespace ServicioPrueba
 
         protected override void OnStart(string[] args)
         {
-                           
+            
 
             // Establece la dirección IP y el puerto en los que el servidor escuchará las solicitudes
             string ipAddress = "127.0.0.1";
@@ -56,7 +57,7 @@ namespace ServicioPrueba
 
         protected override void OnStop()
         {
-
+          
             // Detiene el manejo de solicitudes y cancela la operación asincrónica
             cts.Cancel();
             listener.Stop();
@@ -73,14 +74,22 @@ namespace ServicioPrueba
                 HttpListenerContext context = await listener.GetContextAsync();
 
                 // Verifica el método de la solicitud
-                if (context.Request.HttpMethod == "POST")
-                //  if (context.Request.HttpMethod == "GET")
+                //if (context.Request.HttpMethod == "POST")
+                if (context.Request.HttpMethod == "GET")
+                    
                 {
+
+
+                    // Establece conexion con bb.dd.    
+                      var datos = new Dat();
+                   
+                    //  datos.getJSON(); 
                     // Establece el tipo de contenido de la respuesta como XML
                     context.Response.ContentType = "application/xml";
 
                     // Construye el XML de ejemplo
-                    string xmlResponse = "<root><message>Hola mundo</message></root>";
+                   string xmlResponse = "<root><message>Hola mundo</message></root>";
+                    xmlResponse =  "asdasd";
 
                     // Convierte el XML en bytes
                     byte[] buffer = Encoding.UTF8.GetBytes(xmlResponse);
@@ -89,19 +98,20 @@ namespace ServicioPrueba
                     Stream output = context.Response.OutputStream;
 
                     // Escribe los bytes en el flujo de salida
-                    await output.WriteAsync(buffer, 0, buffer.Length, cancellationToken);
+                   await output.WriteAsync(buffer, 0, buffer.Length, cancellationToken);
 
                     // Escribe los bytes del JSON en el flujo de salida
-                    
+
                     // Establecer el encabezado Content-Type
-                    context.Response.ContentType= "text/JSON";
-                    //get a JSON from the getJSON method and send it to the web browser                   
-                    
+                    //  context.Response.ContentType= "text/JSON";
+                    //get a JSON from the getJSON method and send it to the web browser 
 
-                    // Serializar el objeto JSON y enviarlo al navegador                    
-                    return Json(datos.getJSON(), JsonRequestBehavior.AllowGet);
+                    // Serializar el objeto JSON y enviarlo al navegador
+                    // 
+                    //     byte[] buffer = Encoding.UTF8.GetBytes(datos.getJSON());
+                    //     await output.WriteAsync(buffer, 0, buffer.Length, cancellationToken);
 
- 
+                    //  await output.WriteAsync(datos.getJSON(), 0, buffer.Length, cancellationToken);
 
                     // Cierra el flujo de salida
                     output.Close();
